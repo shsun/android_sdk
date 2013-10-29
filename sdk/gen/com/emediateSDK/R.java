b108ed01037e361662cfad532d93f2a800dea217
Fredrik Hyttnäs-Lenngren <fredrik@mobileinteraction.se>
2013-10-29 10:19:49 +0100

    Updated git-ignore.

--- sdk/gen/com/emediateSDK/R.java
+++ sdk/gen/com/emediateSDK/R.java
@@ -11,13 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static int ic_action_search=0x7f020000;
+        public static int ic_launcher=0x7f020001;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static int app_name=0x7f030000;
     }
     public static final class style {
-        public static final int AppTheme=0x7f040000;
+        public static int AppTheme=0x7f040000;
     }
 }
