63b53dec8d48531c5a2bc1fb0e67571cc8d61324
Fredrik Hyttnäs-Lenngren <fredrik@mobileinteraction.se>
2013-11-29 14:05:36 +0100

    Params are now Parcable.

--- sdk/src/com/emediate/controller/model/Param.java
+++ sdk/src/com/emediate/controller/model/Param.java
@@ -1,7 +1,48 @@
 package com.emediate.controller.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Param {
+public class Param implements Parcelable {
 
-	public String key, value;
+    public static final Parcelable.Creator<Param> CREATOR = new Creator<Param>() {
+
+	@Override
+	public Param[] newArray(int size) {
+	    return new Param[size];
+	}
+
+	@Override
+	public Param createFromParcel(Parcel source) {
+	    return new Param(source);
+	}
+    };
+
+    public String key, value;
+
+    public Param() {
+	
+    }
+    
+    public Param(String key, String value) {
+	this.key = key;
+	this.value = value;
+    }
+
+    private Param(Parcel parcel) {
+	this.key = parcel.readString();
+	this.value = parcel.readString();
+    }
+
+    @Override
+    public int describeContents() {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+	dest.writeString(key);
+	dest.writeString(value);
+    }
 }
